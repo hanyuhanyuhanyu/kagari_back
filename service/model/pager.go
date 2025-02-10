d5908f55c31b38b4070370ba7a2e2fb7def3ded4
umano <concon.wolf@gmail.com>
2025-02-10 16:07:16 +0000

    article/searchのエンドポイントを追加

--- service/model/pager.go
+++ service/model/pager.go
@@ -1,8 +1,26 @@
 package model
 
+import "strconv"
+
 type Pager struct {
 	Limit  uint64
 	Offset uint64
+}
+
+func PagerFromString(l string, o string) (*Pager, error) {
+	p := &Pager{}
+	limit, err := strconv.ParseUint(l, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	p.Limit = limit
+	offset, err := strconv.ParseUint(o, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	p.Offset = offset
+	return p, err
+
 }
 
 func (p *Pager) GetLimit() uint64 {
